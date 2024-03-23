d69a0b2e8840c0e60794280890dc2a37a346e7ff
paianish62 <69522714+paianish62@users.noreply.github.com>
2024-03-23 19:19:50 -0400

    Add files via upload

---
+++ treetry.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from typing import Any, Optional
+class Tree:
+    def __init__(self, root: Optional[Any] = None, subtrees: List[Tree] = None) -> None:
+        self._root = root
+        self._subtrees = subtrees if subtrees is not None else []
+
+    def is_empty(self) -> bool:
+        return self._root is None
+
+    def add_country(self, criteria_path: List[str], country: str):
+        if not criteria_path:
+            self._subtrees.append(Tree(country))
+        else:
+            criterion = criteria_path[0]
+            subtree = self._find_subtree(criterion)
+            if subtree is None:
+                subtree = Tree(criterion)
+                self._subtrees.append(subtree)
+            subtree.add_country(criteria_path[1:], country)
+
+    def _find_subtree(self, criterion: str) -> Optional[Tree]:
+        for subtree in self._subtrees:
+            if subtree._root == criterion:
+                return subtree
+        return None
+
+    def query(self, criteria_path: List[str]) -> List[str]:
+        if not criteria_path:
+            return [subtree._root for subtree in self._subtrees]
+        else:
+            criterion = criteria_path[0]
+            subtree = self._find_subtree(criterion)
+            if subtree is not None:
+                return subtree.query(criteria_path[1:])
+            else:
+                return []#changes
+
+tree = Tree("World")
+tree.add_country(["Europe", "Developed", "Tertiary", "Long Run"], "England")
+tree.add_country(["Asia", "Emerging", "Primary", "Short Run"], "India")
+print(tree.query(["Europe", "Developed", "Tertiary", "Long Run"]))
+
+
+
+# List Contries & Data on interest raes and SDGS
+# for loop on the countries
+# CPI, Interest Rates, GDP
+# Ethical Score
